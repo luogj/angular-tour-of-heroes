bcfe1cd8b7112de1494d025492b9d8f6bcae5b11
David Luo <lgj_sg@hotmail.com>
2016-10-04 17:58:54 +0800

    7. HTTP: completed update() method

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule }					from '@angular/core';
 import { BrowserModule }			from '@angular/platform-browser';
 import { FormsModule }				from '@angular/forms';
+import { HttpModule }				from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } 	from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  	from './in-memory-data.service';
 
 import { routing }					from './app.routing';
 
@@ -15,6 +20,8 @@ import { HeroService }				from './hero.service';
 	imports: [ 
 		BrowserModule,
 		FormsModule,
+		HttpModule,
+		InMemoryWebApiModule.forRoot(InMemoryDataService),
 		routing
 	],
 	declarations: [
